7857ec5e9f2fd44b89c20844025d9b880303f045
TBD <tehya.deardorff@quapawnation.com>
2025-01-28 10:46:15 -0600

    Add files via upload

---
+++ ffmpeg_wav_to_mp3.ps1
@@ -0,0 +1,28 @@
+﻿# Prompt for folder path containing .wav files.
+$sourceFolder = Read-Host 'Input source folder path for .wav files.'
+
+# Prompt destination path for converted .mp3 files.
+$destFolder = Read-Host 'Input destination folder path for converted .mp3 files.'
+
+# Test the destination folder exists, if not, create it.
+if (-not (Test-Path $destinationFolder)) {
+    New-Item -ItemType Directory -Path $destFolder
+}
+
+# Iterate .wav files from the source folder
+$wavFiles = Get-ChildItem -Path $sourceFolder -Filter *.wav
+
+# Loop through each .wav file
+foreach ($wavFile in $wavFiles) {
+    # Define the output .mp3 file path
+    $outputFile = Join-Path -Path $destFolder -ChildPath ($wavFile.BaseName + ".mp3")
+    
+    # Build the FFmpeg command to convert to .mp3
+    $ffmpegCommand = "ffmpeg -i `"$($wavFile.FullName)`" -vn -ar 44100 -ac 2 -b:a 320k `"$outputFile`""
+
+    # Execute and output command
+    Write-Host "Converting $($wavFile.Name) to $outputFile"
+    Invoke-Expression $ffmpegCommand
+}
+
+Write-Host "File conversion is complete for inputted path."
---
+++ sort_to_parent_folder.ps1
@@ -0,0 +1,48 @@
+﻿# Set source folder
+$sourceFolder = "D:\quadict_audio\.files_to_sort"
+
+# Set destination folder possibilities
+$destAB = "D:\quadict_audio\[ab] anna_beaver"
+$destACG = "D:\quadict_audio\[acg] alice_crawfish_gilmore"
+$destFGR = "D:\quadict_audio\[fgr] fannie_goodeagle_richards"
+$destLQ = "D:\quadict_audio\[lq] louis_quapaw"
+$destMLR = "D:\quadict_audio\[mlr] mary_lane_redeagle"
+$destMS = "D:\quadict_audio\[ms] maude_supernaw"
+$destMW = "D:\quadict_audio\[mw] mary_wilson"
+$destOM = "D:\quadict_audio\[om] odestine_mcwatters"
+
+# Iterate only .mp3 files from source folder
+$mp3Files = Get-ChildItem -Path $sourceFolder -Filter *.mp3
+
+# Loop through each .mp3 file
+foreach ($file in $mp3Files) {
+    # Retrieve first two characters from each .mp3 filename
+    $prefix = $file.Name.Substring(0, 2).ToLower()
+
+    # Map the prefix to the corresponding destination folder
+    switch ($prefix) {
+        "ab" { $destinationFolder = $destAB }
+        "ac" { $destinationFolder = $destACG }
+        "fg" { $destinationFolder = $destFGR }
+        "lq" { $destinationFolder = $destLQ }
+        "ml" { $destinationFolder = $destMLR }
+        "ms" { $destinationFolder = $destMS }
+        "mw" { $destinationFolder = $destMW }
+        "om" { $destinationFolder = $destOM }
+        default { 
+            Write-Host "No matching folder found for $($file.Name)"
+            continue
+        }
+    }
+
+     # Verify destination folder existence
+    if (-not (Test-Path -Path $destinationFolder)) {
+        Write-Host "Destination folder not found for $($file.Name)."
+    }
+
+    # Move the file to the folder
+    $destinationPath = Join-Path -Path $destinationFolder -ChildPath $file.Name
+    Move-Item -Path $file.FullName -Destination $destinationPath
+
+    Write-Host "Moved $($file.Name) to $destinationFolder"
+}
